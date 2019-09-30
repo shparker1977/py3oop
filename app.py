239c0be231383413bf4ea3fa04a4265ae2f1d8cd
i81196 <sha.parker@sap.com>
2019-09-30 14:34:57 -0500

    Added xmlrunner

--- app.py
+++ app.py
@@ -12,4 +12,7 @@ def hello_user(username):
     return 'Why Hello %s!\n' % username
 
 if __name__ == '__main__':
+    import xmlrunner
+    runner = xmlrunner.XMLTestRunner(output='test-reports')
+    unittest.main(testRunner=runner)
     app.run(host='0.0.0.0')     # open for everyone
